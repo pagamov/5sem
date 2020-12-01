90d242ee8bbf913dc58fdef30698c91f3394d09f
Pavel Gamov <pagamov@gmail.com>
2020-12-01 17:19:11 +0300

    serega

--- Компьютерная графика /3lab/src/sketch.js
+++ Компьютерная графика /3lab/src/sketch.js
@@ -82,6 +82,53 @@ function drawCylinder(h, r, x, y, z) {
 	}
 }
 
+function drawConus(h, r, x, y, z) {
+	let step = int(document.getElementById('n_number_of_sides').value);
+
+	// y = y * cos(frameCount * 0.1 + y);
+
+	for (var fi = 0; fi < 360; fi += step) {
+		beginShape();
+		vertex(x, y + h / 2, z);
+		vertex(	x + cos(fi / 360 * 2 * PI) * r,
+				y + h / 2,
+				z + sin(fi / 360 * 2 * PI) * r);
+		vertex(	x + cos((fi + step) / 360 * 2 * PI) * r,
+				y + h / 2,
+				z + sin((fi + step) / 360 * 2 * PI) * r);
+		endShape();
+
+		beginShape();
+		vertex(x, y - h / 2, z);
+		vertex(	x + cos(fi / 360 * 2 * PI) * r / 2,
+				y - h / 2,
+				z + sin(fi / 360 * 2 * PI) * r / 2);
+		vertex(	x + cos((fi + step) / 360 * 2 * PI) * r / 2,
+				y - h / 2,
+				z + sin((fi + step) / 360 * 2 * PI) * r / 2);
+		endShape();
+
+		beginShape();
+		vertex(	x + cos(fi / 360 * 2 * PI) * r / 2,
+				y - h / 2,
+				z + sin(fi / 360 * 2 * PI) * r / 2);
+
+		vertex(	x + cos(fi / 360 * 2 * PI) * r,
+				y + h / 2,
+				z + sin(fi / 360 * 2 * PI) * r);
+
+		vertex(	x + cos((fi + step) / 360 * 2 * PI) * r,
+				y + h / 2,
+				z + sin((fi + step) / 360 * 2 * PI) * r);
+
+		vertex(	x + cos((fi + step) / 360 * 2 * PI) * r / 2,
+				y - h / 2,
+				z + sin((fi + step) / 360 * 2 * PI) * r / 2);
+		endShape();
+	}
+}
+
+
 function setup() {
 	createCanvas(WIDTH, HEIGHT, WEBGL);
 	background(200);
@@ -90,19 +137,24 @@ function setup() {
 
 function draw() {
 	// noStroke();
-	stroke(255,69,5);
+	// stroke(255,69,5);
+	stroke(0,0,200);
 	strokeWeight(1);
 
 	background(200);
-	ambientLight(100);
+	ambientLight(0,200,200);
 
 	let x1 = map(mouseX, 0, WIDTH, -200, 200);
 	let x2 = map(mouseY, 0, HEIGHT, 200, -200);
 
 	directionalLight(100, 100, 100, x1, x2, 200);
 	ambientMaterial(255,69,0);
+	// ambientMaterial(100,30,43);
 	orbitControl();
-	push();
-	drawCylinder(300, 200, 0, -20, 0);
-	pop();
+	// drawConus(300, 200, 0, -20, 0);
+	// drawPiramid(300, 200, 300, -20, 300);
+
+	rotateX(frameCount % 600 / 100);
+	rotateY(frameCount % 600 / 100);
+	box(100);
 }
